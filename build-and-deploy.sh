7cd3c53395295c796615f78ba69232b45e4c22a3
rgeral <geral.robin@yahoo.fr>
2024-12-30 21:34:39 +0100

    Adding build script

---
+++ build-and-deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd backend || { echo "Erreur : impossible de se déplacer dans le répertoire backend"; exit 1; }
+
+echo "Exécution de mvn clean install..."
+mvn clean install
+
+if [ $? -eq 0 ]; then
+  echo "Maven build successful, proceeding with frontend build..."
+
+  cd ../frontend || { echo "Erreur : impossible de se déplacer dans le répertoire frontend"; exit 1; }
+  echo "Exécution de npm run build..."
+  npm run build
+
+  if [ $? -eq 0 ]; then
+    echo "Frontend build successful, proceeding with Docker build..."
+
+    cd .. || { echo "Erreur : impossible de revenir dans le répertoire parent"; exit 1; }
+
+    docker-compose up --build
+  else
+    echo "Frontend build failed, aborting Docker build."
+    exit 1
+  fi
+else
+  echo "Maven build failed, aborting Docker build."
+  exit 1
+fi
